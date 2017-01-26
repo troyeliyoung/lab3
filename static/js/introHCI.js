82b1001027f801df99efd5da3beea5027c275db2
Troy Young <t9young@ucsd.edu>
2017-01-26 10:58:47 -0800

    lab3

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,23 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Javascript is working");
+		$("#testjs").text("Connecting...");
+		$(".jumbotron p").toggleClass("active");
 	});
+
+	$("a.thumbnail").click(projectClick);
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+}
+function projectClick(e) {
+	e.preventDefault();
+	var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       $(description).fadeOut()
+    }
 }
